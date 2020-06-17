09b1ae26cf09020a91de90c8f4393d7f83bb3f3a
SamiXSous <SamsonV.Evertsz@Gmail.com>
2020-06-17 15:07:48 +0200

    Update Menu configuration

--- src/Config/menu.php
+++ src/Config/menu.php
@@ -5,7 +5,7 @@ return [
         'key' => 'printful',          // uniquely defined key for menu-icon
         'name' => 'printful',        //  name of menu-icon
         'route' => 'admin.printful.index',  // the route for your menu-icon
-        'sort' => 5,                    // Sort number on which your menu-icon should display
+        'sort' => 3,                    // Sort number on which your menu-icon should display
         'icon-class' => 'printful-icon',   //class of menu-icon
     ],
 ];
